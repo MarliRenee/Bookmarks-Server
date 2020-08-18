9ef53e32e5856c723421197c2f750e605cf8d299
Marli Renee <marlirenee@gmail.com>
2020-08-18 15:05:16 -0700

    GitGuardian recommendaations

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    API_TOKEN: '263601e3-863c-4dd4-8fb9-bee2161d51d8',
+    API_TOKEN: process.env.API_TOKEN,
     DB_URL: process.env.DB_URL || 'postgresql://dunder_mifflin@localhost/bookmarks',
 }
